9eb64b5f8dad6cd938e364cf2f04a58188351a6c
Antony100 <antonyangeli10@gmail.com>
2020-07-09 16:49:32 +0100

    Update settings.py

--- reddithub/reddithub/settings.py
+++ reddithub/reddithub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e#_g!@rv*j7fz#rox%)fnutc!k8b16i(zmugi*z69dhi$w^57o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
